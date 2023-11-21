aae77f298aa446e5cb526b96b277d0ea710077e8
Nathan Krueger <natekrueger805@gmail.com>
2023-11-21 16:34:54 -0700

    Add callbacks

--- ring_downloader/ring_events.py
+++ ring_downloader/ring_events.py
@@ -2,27 +2,35 @@ import time
 import configparser
 import json
 import os
+import tempfile
 import cv2
 import queue
+import playsound
 
 import tensorflow as tf
 import numpy as np
 
 from pathlib import Path
-from threading import Thread
+from threading import Thread, Lock
 from datetime import datetime
 
 from ring_doorbell import Ring, Auth, exceptions
 from oauthlib.oauth2 import MissingTokenError
+from gtts import gTTS
 
 cache_file = Path("ring_downloader/test_token.cache")
 ring_ini_file = os.path.abspath('ring_downloader/ring_api.ini')
 ring = None
 
+callback_queue = queue.Queue()
 evt_queue = queue.Queue()
+callback_lock = Lock()
+
+# a dictionary of event label to list of tuples
+# each tuple is a pair of (function, (arg1, .. argN))
+callbacks_dict = {}
 
 cameras_to_listen_on = ['Garage Cam', 'Shop Floodlight Camera']
-cameras_to_listen_on.append('Upstairs')
 
 def token_updated(token):
     cache_file.write_text(json.dumps(token))
@@ -58,10 +66,26 @@ def on_motion(event):
     if event.kind == 'motion' and device_name in cameras_to_listen_on:
         evt_queue.put(device_name)
 
+def speak(text, tld='en'):
+    with tempfile.NamedTemporaryFile() as tmpfile:
+        gTTS(text=text, lang='en', tld=tld, slow=False).save(tmpfile.name)
+        playsound.playsound(tmpfile.name)
+
+def callback_thread():
+    while True:
+        callback_tup = callback_queue.get()
+        fn = callback_tup[0]
+        args = callback_tup[1]
+        fn(*args)
+
 def predict_thread():
+    global callback_lock
+
+    # initialize the tensorflow model
     model = tf.keras.models.load_model('ring_convnet_weights/checkpt.ckpt')
     labels_to_consider = ['none', 'car', 'dog', 'turkey', 'deer']
     label_dict = {k: v for k, v in enumerate(labels_to_consider)}
+
     while True:
         device_name = evt_queue.get()
         img = retrieve_image(device_name)
@@ -69,7 +93,17 @@ def predict_thread():
         prediction = model.predict(np.expand_dims(img, axis=0))
         int_label = tf.argmax(prediction[0]).numpy()
         label = label_dict[int_label]
-        print(f"----> Yo there's a {label} at your {device_name} !!!")
+
+        # Add our own default callback 
+        message_text = f"There's a {label} at your {device_name}"
+        callback_queue.put((speak, (message_text, 'ie')))
+
+        # guard access to the callback dictionary
+        with callback_lock:
+            if label in callbacks_dict:
+                callbacks_list = callbacks_dict[label]
+                for cb in callbacks_list:
+                    callback_queue.put(cb)
 
 def main():
     # don't use GPU for running one prediction at a time...
@@ -92,12 +126,18 @@ def main():
     ring = Ring(auth)
     ring.update_data()
 
-    # Register the callback function for motion events
+    # register the callback function for motion events
     ring.start_event_listener()
     ring.add_event_listener_callback(on_motion)
 
-    t = Thread(target=predict_thread, daemon=True)
-    t.start()
+    # add a thread which runs the inference and triggers
+    # callbacks to occur on the callback_thread if needed
+    pt = Thread(target=predict_thread, daemon=True)
+    pt.start()
+
+    # respond to any callbacks
+    ct = Thread(target=callback_thread, daemon=True)
+    ct.start()
 
     try:
         # Keep the script running to listen for events
